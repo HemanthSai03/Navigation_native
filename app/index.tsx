0d83b7f8b6f892df093e1e1a05a246127a064901
Hemanth Sai <hemanthsai0119@gmail.com>
2024-08-23 12:13:43 +0530

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,35 @@
+import { Link, useRouter } from "expo-router";
+import { Button, Text, View, StyleSheet, Platform } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./home";
+import Details from "./screens/details";
+import CreatePostScreen from "./screens/createPost";
+
+const router = useRouter();
+
+const Stack = createNativeStackNavigator();
+
+export default function Index() {
+  return (
+    <NavigationContainer independent={true}>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: "Dashboard" }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          initialParams={{ itemId: 42 }}
+        />
+         <Stack.Screen
+          name="CreatePost"
+          component={CreatePostScreen}
+          options={{ title: "write mail" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
